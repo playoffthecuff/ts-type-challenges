9c949a7b6efd418af7c10de2c982ce8aeeb01264
playoffthecuff <jmobile@bk.ru>
2025-10-24 05:13:43 +0300

    add string to union solution

--- playground/medium/00531-medium-string-to-union.ts
+++ playground/medium/00531-medium-string-to-union.ts
@@ -19,7 +19,7 @@
 
 /* _____________ Your Code Here _____________ */
 
-type StringToUnion<T extends string> = any
+type StringToUnion<T extends string> = T extends `${infer First}${infer Rest}` ? First | StringToUnion<Rest> : never;
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
