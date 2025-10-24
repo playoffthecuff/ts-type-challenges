225ab655909f6c9f5e550749d154d3120d2053d1
playoffthecuff <jmobile@bk.ru>
2025-10-24 05:01:42 +0300

    add absolut solution

--- playground/medium/00529-medium-absolute.ts
+++ playground/medium/00529-medium-absolute.ts
@@ -19,7 +19,7 @@
 
 /* _____________ Your Code Here _____________ */
 
-type Absolute<T extends number | string | bigint> = any
+type Absolute<T extends number | string | bigint> = `${T}` extends `-${infer K}` ? K : `${T}`;
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
