e43b5e3bc5294440ac934512281052656e5a43f6
playoffthecuff <jmobile@bk.ru>
2025-10-26 15:07:27 +0300

    add diff solution

--- playground/medium/00645-medium-diff.ts
+++ playground/medium/00645-medium-diff.ts
@@ -12,7 +12,8 @@
 
 /* _____________ Your Code Here _____________ */
 
-type Diff<O, O1> = any
+type MyOmit<T, K extends keyof T> = { [P in keyof T as P extends K ? never : P]: T[P] };
+type Diff<O, O1> = MyOmit<O & O1, keyof (O | O1)>;
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
